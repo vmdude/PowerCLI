7cb720838327bf8e6520c2e30dac6c76bb58bf50
Frederic MARTIN <frederic@martin.lc>
2013-08-09 10:28:24 +0200

    Adding filled up empty tag with 'N/A' function/switch

--- Set-Tags.ps1
+++ Set-Tags.ps1
@@ -21,6 +21,18 @@
 	Description
 	-----------
 	Replicate custom field from VM01 to VM02 (with overwriting destination)
+.EXAMPLE 
+	C:\foo> .\Set-Tags.ps1 -vmDestinationName VM02
+	
+	Description
+	-----------
+	Fill custom field from VM02 with "N/A" string (without overwriting destination)
+.EXAMPLE 
+	C:\foo> .\Set-Tags.ps1 -vmDestinationName VM02 -force
+	
+	Description
+	-----------
+	Fill custom field from VM02 with "N/A" string (with overwriting destination)
 .PARAMETER vmSourceName 
    Name of the VM used for source replication
 .PARAMETER vmDestinationName 
@@ -30,7 +42,6 @@
 #> 
 
 param (
-	[Parameter(Mandatory=$True)]
 	[string]$vmSourceName,
 	[Parameter(Mandatory=$True)]
 	[string]$vmDestinationName,
@@ -48,10 +59,12 @@ $htCustomFields = @{}
 Get-View CustomFieldsManager-CustomFieldsManager | %{$_.field} | ?{$_.managedObjectType -match "VirtualMachine|^$"} | %{$htCustomFields.Add($_.Key,$_.Name)}
 
 # Check for source VM
-$vmSource = Get-View -ViewType VirtualMachine -Filter @{"Name"="^$vmSourceName$"} -Property Config.Annotation,Value
-if (($vmSource | Measure-Object).Count -eq 0) {
-	Write-Host -Foreground Red "[ERROR] Source VM $vmSourceName doesn't exist."
-	Break
+if ($vmSourceName) {
+	$vmSource = Get-View -ViewType VirtualMachine -Filter @{"Name"="^$vmSourceName$"} -Property Config.Annotation,Value
+	if (($vmSource | Measure-Object).Count -eq 0) {
+		Write-Host -Foreground Red "[ERROR] Source VM $vmSourceName doesn't exist."
+		Break
+	}
 }
 
 # Check for destination VM
@@ -62,32 +75,62 @@ if (($vmDestination | Measure-Object).Count -eq 0) {
 }
 
 # Annotation handler
-if ($vmSource.Config.Annotation.length -gt 0 -And $vmSource.Config.Annotation -ne $vmDestination.Config.Annotation) {
+if (-Not $vmSourceName) {
 	if ($vmDestination.Config.Annotation -And -Not $force) {
 		Write-Host -Foreground Red "[ERROR] Annotation in destination VM"($vmDestination.Name)"is not empty. Use -force switch to overwrite."
 	} else {
 		try {
 			$vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
-			$vmConfigSpec.Annotation = $vmSource.Config.Annotation
+			$vmConfigSpec.Annotation = "N/A"
 			$vmDestination.ReconfigVM($vmConfigSpec)
-			Write-Host -Foreground Green "[SUCCESS] Annotation in destination VM"($vmDestination.Name)"has been cloned."
+			Write-Host -Foreground Green "[SUCCESS] Annotation in destination VM"($vmDestination.Name)"has been filled up with 'N/A'."
 		} catch [System.Exception] {
-			Write-Host -Foreground Red "[ERROR] Annotation in destination VM"($vmDestination.Name)"cannot be cloned: "($_.Exception.ToString())
+			Write-Host -Foreground Red "[ERROR] Annotation in destination VM"($vmDestination.Name)"cannot be filled: "($_.Exception.ToString())
+		}
+	}
+} else {
+	if ($vmSource.Config.Annotation.length -gt 0 -And $vmSource.Config.Annotation -ne $vmDestination.Config.Annotation) {
+		if ($vmDestination.Config.Annotation -And -Not $force) {
+			Write-Host -Foreground Red "[ERROR] Annotation in destination VM"($vmDestination.Name)"is not empty. Use -force switch to overwrite."
+		} else {
+			try {
+				$vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
+				$vmConfigSpec.Annotation = $vmSource.Config.Annotation
+				$vmDestination.ReconfigVM($vmConfigSpec)
+				Write-Host -Foreground Green "[SUCCESS] Annotation in destination VM"($vmDestination.Name)"has been cloned."
+			} catch [System.Exception] {
+				Write-Host -Foreground Red "[ERROR] Annotation in destination VM"($vmDestination.Name)"cannot be cloned: "($_.Exception.ToString())
+			}
 		}
 	}
 }
 
 # Custom fields handler
-foreach ($vmField in $vmSource.Value) {
-	if ($vmField.Value.length -gt 0 -And $vmField.Value -ne ($vmDestination.Value | ?{$_.Key -eq $vmField.Key}).Value) {
-		if (($vmDestination.Value | ?{$_.Key -eq $vmField.Key}).Value -And -Not $force) {
-			Write-Host -Foreground Red "[ERROR] Custom Field"($htCustomFields[$vmField.Key])"in destination VM"($vmDestination.Name)"is not empty. Use -force switch to overwrite."
+if (-Not $vmSourceName) {
+	foreach ($vmField in $htCustomFields.Keys) {
+		if (($vmDestination.Value | ?{$_.Key -eq $vmField}).Value -And -Not $force) {
+			Write-Host -Foreground Red "[ERROR] Custom Field"($htCustomFields[$vmField])"in destination VM"($vmDestination.Name)"is not empty. Use -force switch to overwrite."
 		} else {
 			try {
-				$vmDestination.setCustomValue($htCustomFields[$vmField.Key],$vmField.Value)
-				Write-Host -Foreground Green "[SUCCESS] Custom Field"($htCustomFields[$vmField.Key])"in destination VM"($vmDestination.Name)"has been cloned."
+				$vmDestination.setCustomValue($htCustomFields[$vmField],"N/A")
+				Write-Host -Foreground Green "[SUCCESS] Custom Field"($htCustomFields[$vmField])"in destination VM"($vmDestination.Name)"has been filled up with 'N/A'."
 			} catch [System.Exception] {
-				Write-Host -Foreground Red "[ERROR] Custom Field"($htCustomFields[$vmField.Key])"in destination VM"($vmDestination.Name)"cannot be cloned:"($_.Exception.ToString())
+				Write-Host -Foreground Red "[ERROR] Custom Field"($htCustomFields[$vmField])"in destination VM"($vmDestination.Name)"cannot be filled:"($_.Exception.ToString())
+			}
+		}
+	}
+} else {
+	foreach ($vmField in $vmSource.Value) {
+		if ($vmField.Value.length -gt 0 -And $vmField.Value -ne ($vmDestination.Value | ?{$_.Key -eq $vmField.Key}).Value) {
+			if (($vmDestination.Value | ?{$_.Key -eq $vmField.Key}).Value -And -Not $force) {
+				Write-Host -Foreground Red "[ERROR] Custom Field"($htCustomFields[$vmField.Key])"in destination VM"($vmDestination.Name)"is not empty. Use -force switch to overwrite."
+			} else {
+				try {
+					$vmDestination.setCustomValue($htCustomFields[$vmField.Key],$vmField.Value)
+					Write-Host -Foreground Green "[SUCCESS] Custom Field"($htCustomFields[$vmField.Key])"in destination VM"($vmDestination.Name)"has been cloned."
+				} catch [System.Exception] {
+					Write-Host -Foreground Red "[ERROR] Custom Field"($htCustomFields[$vmField.Key])"in destination VM"($vmDestination.Name)"cannot be cloned:"($_.Exception.ToString())
+				}
 			}
 		}
 	}
